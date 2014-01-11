a667dec1b39ebcf6a0be47f173885279da994507
Ivan Toledo <itoledo2404@gmail.com>
2014-01-11 12:22:21 +0100

    Tercer Cambio
    Cambio el valor de la impresion

--- HolaMundo/src/Programa.java
+++ HolaMundo/src/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Creo una variable de tipo string
-		String a= "Hola Mundo";
+		String a= "Buenos dias";
 		System.out.println(a);
 	}
 
